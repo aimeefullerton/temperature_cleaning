70837d48a1f74ab2ac3ff7a1ac7c5a68166d3099
Aimee Fullerton <aimee.fullerton@gmail.com>
2021-09-23 13:44:54 -0700

    Apply same updates as for Wenatchee air file, moving code into functions in cleaning_functions.R.

--- code/clean_Snoqualmie_water.R
+++ code/clean_Snoqualmie_water.R
@@ -1,44 +1,45 @@
 # Clean temperature data files to remove erroneous readings (e.g. from air, sediment, or ice)
-# Aimee H Fullerton, 18 November 2020
+# Aimee H Fullerton, 22 September 2021
 
+# SETUP ####
 # Load functions
 numdailyobs <- 48
 source("code/cleaning_functions.R")
 
 # Directories
 watershed <- "snoqualmie"
-first.year <- 2019
+first.year <- 2020
 date.begin <- "-09-01"
 date.end <- "-08-31"
 data.dir <- "/Users/aimee_fullerton/OneDrive/Work/Research/StreamTemperature/Hobonet/Data"
 raw.data.folder <- paste0("Data_Raw_Sep", (first.year + 1), "/water")
 old.data.folder <- paste0("Data_Raw_Sep", first.year, "/water")
 cleaned.data.folder <- paste0("Data_Cleaned_", (first.year + 1), "/water")
+if(!dir.exists(paste0(data.dir, "/", cleaned.data.folder))){
+  dir.create(paste0(data.dir, "/Data_Cleaned_", (first.year + 1)), showWarnings = F)
+  dir.create(paste0(data.dir, "/", cleaned.data.folder), showWarnings = F)
+}
+oldfiles <- dir(paste0(data.dir, "/", old.data.folder))
+thefiles <- dir(paste0(data.dir, "/", raw.data.folder))
+oldfiles <- toupper(oldfiles); thefiles <- toupper(thefiles)
 
-# Clean the data ####
-(thefiles <- dir(paste0(data.dir, "/", raw.data.folder)))
-(oldfiles <- dir(paste0(data.dir, "/", old.data.folder)))
-
-i <- 1 # Look at 'thefiles' and pick sites one by one manually
-
+# Clean the data, processing sites individually ####
+thefiles # Look at 'thefiles' and pick sites one manually
+i <- 1
 while(!is.null(i)){
   data.file <- thefiles[i]
   site <- gsub("_.*","", data.file)
   cat(site, "\n")
   
   # Prepare possible files to use
-  old.loggers <- oldfiles[grep(paste0(site, "_"), oldfiles)]
-  new.loggers <- thefiles[grep(paste0(site, "_"), thefiles)]
-  old.loggers <- old.loggers[which(gsub("_", "", substr(old.loggers,1,3)) == site)]
-  new.loggers <- new.loggers[which(gsub("_", "", substr(new.loggers,1,3)) == site)]
-  cat("Last year's file(s): ", old.loggers, "\n")
-  cat("This year's file(s): ", new.loggers, "\n")
+  old.loggers <- oldfiles[grep(site, oldfiles)]
+  new.loggers <- thefiles[grep(site, thefiles)]
   old.list <- new.list <- NULL
   
   # Read in and prepare data from previous year at this site
   if(length(old.loggers) > 0){
     for(j in 1:length(old.loggers)){
-      td <- prepare.file(data.file = old.loggers[j], directory = paste0(data.dir, "/", old.data.folder))
+      td <- prepare.file(data.file = old.loggers[j], directory = paste0(data.dir, "/", old.data.folder), numdailyobs = numdailyobs)
       old.list <- c(old.list, paste0(site, ".", j))
       assign(paste0(site, ".", j), td)
     }
@@ -46,19 +47,21 @@ while(!is.null(i)){
   
   # Read in and prepare data from current year at this site
   for(j in 1:length(new.loggers)){
-    td <- prepare.file(data.file = new.loggers[j], directory = paste0(data.dir, "/", raw.data.folder))
+    td <- prepare.file(data.file = new.loggers[j], directory = paste0(data.dir, "/", raw.data.folder), numdailyobs = numdailyobs)
     new.list <- c(new.list, paste0(site, ".", j + length(old.loggers)))
     assign(paste0(site, ".", j + length(old.loggers)), td)
   }
   
   # Choose files to stitch together based on dates printed from above steps:
   if(length(old.loggers) > 0){
+    cat("Last year's file(s): ", old.loggers, "\n")
     oldfile2keep <- choose.file("past.year")
   } else {
     print("This site did not have a file last year.")
     oldfile2keep <- NA
   }
   if(length(new.loggers) > 0){
+    cat("This year's file(s): ", new.loggers, "\n")
     newfile2keep <- choose.file("current.year")
   } else{
     print("This site did not have a file this year.")
@@ -97,123 +100,14 @@ dat <- clean.middle(dat, thedirectory)
 plot(dat$Date, dat$Temp, type = 'l', ylab = "Temperature (C)", xlab = "Date")
 summary(dat[!is.na(dat$Temp),])
 
-write.csv(dat, paste0(data.dir, "/", cleaned.data.folder, "/", data.file), row.names = F)
+write.csv(dat, paste0(data.dir, "/", cleaned.data.folder, "/", site, ".csv"), row.names = F)
+rm(list = old.list); rm(list = new.list); rm(td, dat, newfile2keep, oldfile2keep)
+i <- NULL
+cat(paste0("All done with ", site, "!"), "\n")
 }
 
-# Organize data as matrix with Date and Site columns; NAs where no data ####
-  # helpful for use in SSN models
-  (thefiles <- dir(paste0(data.dir, "/", cleaned.data.folder)))
-  
-  dates <- seq(from = as.Date(paste0(first.year, date.begin)), to = as.Date(paste0(first.year + 1, date.end)), by = 1)
-  new.df <- data.frame(matrix(NA, nrow = length(dates) * numdailyobs, ncol = 2))
-  colnames(new.df) <- c("Date", "Time")
-  new.df$Date <- rep(dates, numdailyobs)
-  new.df <- new.df[order(new.df$Date),]
-  if(numdailyobs == 48){
-    new.df$Time <- rep(seq(0, 23.5, 0.5), length(dates))
-  } else{
-    new.df$Time <- rep(seq(0, 23, 1), length(dates))
-  }
-  foo <- paste0(new.df$Date, " ", sprintf("%02d", floor(new.df$Time)), ":00")
-  if(numdailyobs == 48) foo[seq(2, length(foo), 2)] <- gsub(":00", ":30", foo[seq(2, length(foo), 2)])
-  new.df$DateTime <- as.POSIXlt( foo, format = "%Y-%m-%d %H:%M")
-  row.names(new.df) <- NULL
-  
-  for(i in 1:length(thefiles)){
-    site.name <- gsub("_.*","", thefiles[i])
-    td <- read.csv(paste0(data.dir, "/", cleaned.data.folder, "/", thefiles[i]), header = T, stringsAsFactors = F)
-    # Add "DateTime" column if needed
-    if(!"DateTime" %in% colnames(td)){
-      foo <- paste0(td$Date, " ", sprintf("%02d", floor(td$Time)), ":00")
-      if(numdailyobs == 48) foo[seq(2, length(foo), 2)] <- gsub(":00", ":30", foo[seq(2, length(foo), 2)])
-      td$DateTime <- as.POSIXlt( foo, format = "%Y-%m-%d %H:%M")
-      rm(foo)
-      colnames(td)[3] <- "Temp"
-    }
-    new.df <- merge(new.df, td[, c("DateTime", "Temp")], by = c("DateTime"), all.x = T)
-    colnames(new.df)[i + 3] <- site.name
-  }
-  write.csv(new.df, paste0(data.dir, "/Data_Cleaned_", (first.year + 1), "/", watershed, ".wt.", (first.year + 1), ".csv"), row.names = F)
-  
-  # plot in individual panels to check
-  png(paste0(data.dir, "/Data_Cleaned_", (first.year + 1), "/", watershed, ".wt.", (first.year + 1), ".png"), width = 16, height = 10, units = "in", res = 300)
-  par(mfrow = c(6,8), las = 1, cex = 0.5)
-  for(i in 4:(ncol(new.df))){
-    plot(new.df$Date, new.df[,i], type = 'l', ylim = c(-5, 25), main = colnames(new.df)[i], xlab = "", ylab = "")
-  }
-  dev.off()  
-
-
+# Create a matrix of this year's data and plot each site's time series individually ####
+create.matrix(type = "wt", data.dir, cleaned.data.folder, watershed, first.year, date.begin, date.end, numdailyobs, ylm = c(-5, 30))
 
 # Merge with all other years ####
-yy <- first.year + 1
-  
-# wt.all <- read.csv(paste0(data.dir, "/", "WT.2012-2019.csv"), header = T, row.names = 1)
-# sites <- c("C1", "E1", "D1", "G1", "F1", "F1.r", "I1", "L1", "L3", "B1", "L2", "X1", "C2",
-#                "MF2", "MF1", "MF4", "MF3", "MF5", "MF6", "MS5", "MS6", "MS9", "MS10", "MS10.r",
-#                "MS4", "NF3", "NF1", "NF2", "NF4", "R5b", "R5a", "R1", "R4", "R5c", "R2", "R3",
-#                "SF3", "SF1", "SF2", "M1", "S1", "Y1", "K1", "MS8", "T2", "MS7", "T4", "T1")
-# colnames(wt.all)[3:ncol(wt.all)] <- sites
-# wt.all$Time <- wt.all$Time / 2 - 0.5
-# # Add "DateTime" column
-# if(!"DateTime" %in% colnames(wt.all)){
-#   foo <- paste0(wt.all$Date, " ", sprintf("%02d", floor(wt.all$Time)), ":00")
-#   if(numdailyobs == 48) foo[seq(2, length(foo), 2)] <- gsub(":00", ":30", foo[seq(2, length(foo), 2)])
-#   wt.all$DateTime <- as.POSIXlt( foo, format = "%Y-%m-%d %H:%M")
-#   rm(foo)
-# }
-# wt.all <- wt.all[,c("DateTime", "Date", "Time", sites)]  
-# wt.all$DateTime <- as.character(wt.all$DateTime)
-
-wt.all <- read.csv(paste0(data.dir, "/", watershed, ".wt.allyears.csv"), header = T)
-
-wt.all$Date <- as.Date(wt.all$Date)
-sites <- colnames(wt.all[4:ncol(wt.all)])
-sites <- gsub("_.*","", sites)
-wt.all<- wt.all[,c("DateTime", "Date", "Time", sort(colnames(wt.all)[4:ncol(wt.all)]))]
-
-wt.yy <- read.csv(paste0(data.dir, "/Data_Cleaned_", yy, "/", watershed, ".wt.", yy, ".csv"), header = T)
-wt.yy$Date <- as.Date(wt.yy$Date)
-for(i in 4:ncol(wt.yy)){
-  cn <- colnames(wt.yy)[i]
-  colnames(wt.yy)[i] <- gsub("_.*","", cn)
-}
-wt.yy<- wt.yy[,c("DateTime", "Date", "Time", sort(colnames(wt.yy)[4:ncol(wt.yy)]))]
-
-
-# Merge this year with previous years
-thesites <- sort(intersect(sites, colnames(wt.yy)))
-wt.all.merged <- matrix(NA, nrow = nrow(wt.all) + nrow(wt.yy), ncol = (length(sites) + 3))
-wt.all.merged <- as.data.frame(wt.all.merged)
-colnames(wt.all.merged) <- c("DateTime", "Date", "Time", sort(sites))
-wt.all.merged$Date <- as.Date("2000-01-01", "%Y-%m-%d")
-
-wt.all.merged[1:nrow(wt.all),] <- wt.all
-
-idx <- ((nrow(wt.all) + 1) : nrow(wt.all.merged))
-wt.all.merged$Date[idx] <- wt.yy$Date
-wt.all.merged$Time[idx] <- wt.yy$Time
-wt.all.merged$DateTime[idx] <- wt.yy$DateTime
-for(s in 1:length(thesites)){
-  site <- thesites[s]
-  wt.all.merged[idx, site] <- wt.yy[,site]
-}
-
-wt.all.merged <- wt.all.merged[order(wt.all.merged$Date, wt.all.merged$Time),]
-
-summary(wt.all.merged)
-plot(wt.all.merged$Date, wt.all.merged$D1, type = 'l')
-
-write.csv(wt.all.merged, paste0(data.dir, "/", watershed, ".wt.allyears.csv"), row.names = F)
-
-# plot in individual panels to check
-png(paste0(data.dir, "/", watershed, ".wt.allyears.png"), width = 16, height = 10, units = "in", res = 300)
-par(mfrow = c(6,8), las = 1, cex = 0.5)
-
-for(i in 4:(ncol(wt.all.merged))){
-  plot(wt.all.merged$Date, wt.all.merged[,i], type = 'l', ylim = c(-5, 25), main = colnames(wt.all.merged)[i], xlab = "", ylab = "")
-}
-dev.off()  
-
-
-
+update.allyears(type = "wt", data.dir, watershed, first.year, ylm = c(-5, 30))
